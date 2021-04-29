8ecd0de85a396e85e04e427965c9287601108113
Christian Bermeo <czaudiovisual@hotmail.com>
2021-04-29 00:48:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+drivers[0];
+
+
+// function findMatching(names){
+//     for (const drivers of names){
+//         console.log(drivers.names);
+//     }
+// }
+// findMatching(drivers);
