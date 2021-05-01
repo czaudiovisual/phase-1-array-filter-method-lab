38bc9c1aee6df352dc0c8c513b86f4a505e60539
Christian Bermeo <czaudiovisual@hotmail.com>
2021-05-01 19:27:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 // Code your solution here
-const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
-drivers[0];
 
 
-// function findMatching(names){
-//     for (const drivers of names){
-//         console.log(drivers.names);
-//     }
+// function findMatching(driver, string){
+//     return driver.filter(function(name){
+//         return name.toLowerCase().indexOf(string.toLowerCase()) !== -1
+//     })
 // }
-// findMatching(drivers);
+// console.log(findMatching(drivers, 'Bobby'))
+
+
+function findMatching(drivers, names){
+    return drivers.filter(function(name){
+        return name.toLowerCase().indexOf(names.toLowerCase()) === 0;
+    })
+}
+findMatching(drivers, 'Bobby');
+
+
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().indexOf(letters.toLowerCase()) === 0;
+    });
+}
+
+fuzzyMatch(drivers, 'Sa'); 
+
+
+
+
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  function matchName(drivers, driverName) {
+    return drivers.filter(driver => driver.name === driverName)
+  }
+
+matchName(drivers, 'Bobby');
